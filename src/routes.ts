feff5996b18aced519062a21bf9a25c39b28e1e7
Marcio Sardou <marciosut1996@gmail.com>
2023-01-20 17:43:06 -0300

    finalizando as rotas

--- src/routes.ts
+++ src/routes.ts
@@ -38,7 +38,6 @@ export async function appRoutes(app: FastifyInstance) {
     const parsedDate = dayjs(date).startOf("day")
     const weekDay = dayjs(date).get("day")
 
-    console.log("🚀 ", date, weekDay)
     //todos habitos possiveis
     //habitos que ja foram completados
 
@@ -65,9 +64,93 @@ export async function appRoutes(app: FastifyInstance) {
     const completedHabits = day?.dayHabits.map((dayHabit) => {
       return dayHabit.habit_id
     })
+    console.log(
+      "🚀 ~ file: routes.ts:67 ~ completedHabits ~ completedHabits",
+      completedHabits
+    )
     return {
       possibleHabits,
       completedHabits,
     }
+  })
+
+  app.patch("/habits/:id/toggle", async (request) => {
+    //rota para completar ou descompletar o hábito
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = toggleHabitParams.parse(request.params)
+
+    const today = dayjs().startOf("day").toDate()
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    })
+
+    if (dayHabit) {
+      //remover a marcação de completo
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      })
+    } else {
+      //completar o habito
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      })
+    }
+  })
+
+  app.get("/summary", async (request) => {
+    // query mais complexa, mais condições - sql na mão ( raw sql)
+    //sqlite
+    //* SUB QUERY ( UMA QUERY DENTRO DE OUTRA ) */
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id,
+        D.date,
+        (
+          SELECT 
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HWD
+          JOIN habits H
+            ON H.id = HWD.habit_id
+          WHERE
+            HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <=D.date
+        ) as amount
+      FROM days D
+    `
+    return summary
   })
 }
